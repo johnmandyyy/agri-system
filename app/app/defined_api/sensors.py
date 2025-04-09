a999722ab8b07f58169e4d6a7f9117bed3b3d4f4
John Mandy <69840834+johnmandyyy@users.noreply.github.com>
2025-04-09 17:12:52 +0800

    Added hot reload for home.

--- app/app/defined_api/sensors.py
+++ app/app/defined_api/sensors.py
@@ -41,8 +41,15 @@ class Sensor(APIView):
         anemometer = request.data.get('anemometer', 0)
         rainfall = request.data.get('rainfall', 0)
         humidity = request.data.get('humidity', 0)
+
         humidity = self.universal_mapping(humidity, old_min=0, old_max=100, new_min=0, new_max=999)
+        rainfall = self.universal_mapping(rainfall, old_min=0, old_max=100, new_min=0, new_max=999)
+        anemometer = self.universal_mapping(anemometer, old_min=0, old_max=100, new_min=0, new_max=999)
+        
         print(humidity, "Mapped Humidity")
+        print(rainfall, "Mapped Rainfall")
+        print(anemometer, "Mapped Anemometer")
+
         message = 'No storm.'
         remark = 0
 
